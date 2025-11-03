bf48868d9266cbe6aa2ee4b92a6ca30b681b60da
Narthana Sathsara <narthanasathsara93@gmail.com>
2025-11-03 16:57:23 +0900

    added e fls

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+
+export const environment = {
+  production: true,
+  SUPABASE_URL: 'https://test.supabase.co',
+  SUPABASE_ANON_KEY: 'test.test2.test3',
+  SUPABASE_STORAGE_URL: 'https://test.supabase.co/storage/v1/object/public/test/',
+  SUPABASE_BUCKET_NAME: 'test',
+  SUPABASE_MERITS_TABLE: 'test'
+};
